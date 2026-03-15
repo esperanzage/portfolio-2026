d5d1d8d0f6b18a6778f7e49767abe7e759b6bba9
Mathias Rabellino <mrabellino95@gmail.com>
2026-03-15 20:53:55 -0300

    fix base path in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function ScrollToTop() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
